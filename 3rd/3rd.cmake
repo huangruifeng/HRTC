fab8ede2945967af0793c8e90f9e171b6ac7ad20
huangruifeng <200705057hrf@163.com>
2024-11-04 02:47:03 +0800

    [add] 3rd增加x264 与msgpack 源码

--- 3rd/3rd.cmake
+++ 3rd/3rd.cmake
@@ -1,2 +1,12 @@
 add_subdirectory(3rd/libyuv)
 include_directories(3rd/libyuv/include)
+
+add_subdirectory(3rd/msgpack-c)
+include_directories(3rd/msgpack-c/include)
+
+if(HRTC_WINDOWS)
+    add_definitions(-D SYS_WINDOWS)
+endif()
+
+add_subdirectory(3rd/x264)
+include_directories(3rd/x264)
